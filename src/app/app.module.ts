f24ae7a926cec21729f13bdc94009bf72977e65e
Maroo <maros372@gmail.com>
2024-12-02 22:34:00 +0100

    Kinda works este treba opravit vo vytvorenie userov

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { PostService } from './Services/post.service';
 
 @NgModule({
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes), HttpClientModule],
+  providers: [PostService],
 })
 export class AppModule {}
